cdd5e2c26a8826eb2ca8ca02783e268b5211a0cf
PinkNightmare1234 <72984396+PinkNightmare1234@users.noreply.github.com>
2020-10-30 09:13:04 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,14 +3,55 @@ using namespace std ;
 typedef long long ll ;
 
 
-void mergesort(int *a,int start,int end){
+void merge(int *a,int start,int mid,int end){
 
+	int temp[end-start+1] ;
+
+	int i=start ;
+	int j=mid+1 ;
+	int k=0 ;
+
+	while(i<=mid&&j<=end){
+
+		if(a[i]>=a[j]){
+			temp[k]=a[j] ;
+			j++ ;
+			k++ ;
+		}
+		else{
+			temp[k]=a[i] ;
+			i++ ;
+			k++ ;
+		}
+
+	}
+
+	while(i<=mid){
+			temp[k]=a[i] ;
+			i++ ;
+			k++ ;
+	}
+
+	while(j<=end){
+			temp[k]=a[j] ;
+			j++ ;
+			k++ ;
+		}
+
+for(int i=start;i<=end;i++){
+	a[i]=temp[i-start];
+}
+
+
+}
+	
 if(start<end)
 	{
 	int mid=(start+end)/2 ;
 	mergesort(a,start,mid) ;
 	mergesort(a,mid+1,end) ;
-
+	merge(a,start,mid,end) ;
+	
 	}
 
 }
